6032e20f5d39884288de5546a94753ee5c41c703
Andrei G <guzanovand@gmail.com>
2022-02-02 17:55:05 +0700

    Impl methods in ModelImpl

--- src/main/java/Model/ModelImpl.java
+++ src/main/java/Model/ModelImpl.java
@@ -1,4 +1,45 @@
 package Model;
 
-public class ModelImpl {
+public class ModelImpl implements Model {
+    private Tile[][] gameTiles;
+    private static final int FIELD_SIZE = 4;
+
+    private int score = 0;
+    private int maxTailValue = 0;
+
+
+    @Override
+    public void resetGameTiles() {
+
+    }
+
+    @Override
+    public void left() {
+
+    }
+
+    @Override
+    public void right() {
+
+    }
+
+    @Override
+    public void up() {
+
+    }
+
+    @Override
+    public void down() {
+
+    }
+
+    @Override
+    public Tile[][] getGameTiles() {
+
+    }
+
+    @Override
+    public boolean canMove() {
+
+    }
 }
